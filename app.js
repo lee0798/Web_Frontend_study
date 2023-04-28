0fec8b1ec1222e05fccc1fccbea95b860d00d5de
lee0798 <71830104+lee0798@users.noreply.github.com>
2023-04-29 01:53:09 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,16 @@
-/*//print
-console.log()
-//variable: const let var
-const a=5;
-const b=2;
-const MyName="dohwan";
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello "+MyName);
-let MyName="dohwan";
-console.log("hello "+MyName);
-MyName="dohwanee"; 
-console.log("hello "+MyName);
-//boolean true false null 
-const amIFat = true;
-const amIFat1 = null;
-let amIFat2 ;//undefined
-console.log(amIFat);
-//array
-const daysOfWeek=["mon","tue","wed","thu","fri","sat"];
-//get item from array
-console.log(daysOfWeek[5]);
-//add one more day to the array
-daysOfWeek.push("sun");
-//Objects
-const player ={
-    name:"nico",
-    points:10,
-    handsome:false,
-    fat:"little fat"
-};
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
-player.fat=true;//player가 const여도 object는 변경 가능
-player.lastName="Dohwan";//새로운 object 항목 추가
-//function
-function sayHello(nameOfPerson, age){
-    console.log("Hello "+nameOfPerson+  " "+age);
-}
-sayHello("nico",10);*/
-
-const player ={
-    name:"nico",
-    sayHello:function(otherPerson){
-        console.log("hela" + otherPerson+" nice to meet you")
+const h1=document.querySelector("div.hello:first-child h1");
+function handleTitleClick(){
+    if(h1.className==="active"){
+        h1.className="";
+    }else{
+        h1.className="active";
     }
-};
-console.log(player.name);
-player.sayHello("lynn");
+
+}
+
+h1.addEventListener("click",handleTitleClick);
+
+
 
 
 
